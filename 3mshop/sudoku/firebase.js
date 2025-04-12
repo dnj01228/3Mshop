b171938fb0f1741ba6d58c0e7cc49a4c7665fae1
dnj01228 <60135225+dnj01228@users.noreply.github.com>
2025-04-13 01:48:07 +0900

    test

--- 3mshop/sudoku/firebase.js
+++ 3mshop/sudoku/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyDuqDgA4PuS5KZEGSJWTek-6IEcNyC1Jws",
+  authDomain: "mshop-cbe04.firebaseapp.com",
+  projectId: "mshop-cbe04",
+  storageBucket: "mshop-cbe04.firebasestorage.app",
+  messagingSenderId: "341787310581",
+  appId: "1:341787310581:web:a0545b8ed35ec5033f08bc"
 };
 
 const app = initializeApp(firebaseConfig);
