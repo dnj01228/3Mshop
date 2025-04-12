5cbadf2d22cde1a035ef14b084e9fb5129318c3c
dnj01228 <60135225+dnj01228@users.noreply.github.com>
2025-04-13 02:43:36 +0900

    api키 삭제

--- 3mshop/sudoku/firebase.js
+++ 3mshop/sudoku/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDuqDgA4PuS5KZEGSJWTek-6IEcNyC1Jws",
-  authDomain: "mshop-cbe04.firebaseapp.com",
-  projectId: "mshop-cbe04",
-  storageBucket: "mshop-cbe04.firebasestorage.app",
-  messagingSenderId: "341787310581",
-  appId: "1:341787310581:web:a0545b8ed35ec5033f08bc"
+  
 };
 
 const app = initializeApp(firebaseConfig);
